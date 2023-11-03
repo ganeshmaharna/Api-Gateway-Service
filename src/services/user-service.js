16fe4d63c49d87f524178b56ea5a1fc34e1385be
Ganesh Maharna <ganeshmaharna@gmail.com>
2023-11-03 18:37:03 +0530

    Added the signin logic and after successful signin generated a jwt token

--- src/services/user-service.js
+++ src/services/user-service.js
@@ -1,6 +1,7 @@
 const { StatusCodes } = require('http-status-codes');
 const { UserRepository } = require('../repositories');
 const AppError = require('../utils/errors/app-error');
+const {Auth} = require("../utils/common");
 const userRepository = new UserRepository();
 
 async function create(data) {
@@ -18,7 +19,29 @@ async function create(data) {
         throw new AppError('Cannot create a new user object', StatusCodes.INTERNAL_SERVER_ERROR);
     }
 }
+async function signin(data){
+    try {
+        const user = await userRepository.getUserByEmail(data.email);
+        console.log("This is Auth ",Auth);
+        if(!user) {
+            throw new AppError('No user found for the given email', StatusCodes.NOT_FOUND);
+        }
+        const passwordMatch = Auth.checkPassword(data.password, user.password);
+        console.log("password match", passwordMatch)
+        if(!passwordMatch) {
+            throw new AppError('Invalid password', StatusCodes.BAD_REQUEST);
+        }
+        const jwt = Auth.createToken({id: user.id, email: user.email});
+        console.log("This is the jwt token ",jwt);
+        return jwt;
+    } catch (error) {
+        if(error instanceof AppError) throw error;
+        console.log(error);
+        throw new AppError('Something went wrong', StatusCodes.INTERNAL_SERVER_ERROR);
+    }
+}
 
 module.exports = {
-    create
+    create,
+    signin
 }
